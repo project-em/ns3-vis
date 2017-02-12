4d1337bd5dabdc907a7dbe6095e0c62be17edfcf
Sacha Best <sachabest@outlook.com>
2017-02-12 14:19:13 -0500

    switch to pg

--- db/schema.js
+++ db/schema.js
@@ -1,12 +1,83 @@
-var mongoose = require('mongoose');
+var Sequelize = require('sequelize');
 
 var exports = module.exports = {};
 var models = exports.models = {};
 
-var song = new mongoose.Schema({
-    song_id: String,
-    artist: String,
-    title: String
+exports.db = new Sequelize('prod', process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DATABASE_URL,
+  dialect: 'postgres'
 });
 
-models.popular_songs = mongoose.model('song', song);
+models.source = exports.db.define('source', {
+    id: {
+        type: Sequelize.UUID
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    logo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+}, { freezeTableName: true });
+
+models.topic = exports.db.define('topic;', {
+    id: Sequelize.UUID,
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+}, { freezeTableName: true });
+
+models.article = exports.db.define('article', {
+    id: {
+        type: Sequelize.UUID
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    body: {
+        type: Sequelize.STRING,
+    },
+    topic: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+            model: models.topic,
+            key: 'id'
+        }
+    },
+    url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    source_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+            model: models.source,
+            key: 'id'
+        }
+    }
+}, { freezeTableName: true });
+
+models.sentence = exports.db.define('sentence', {
+    id: Sequelize.UUID,
+    article: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+            model: models.article,
+            key: 'id'
+        }
+    },
+    bias: Sequelize.INTEGER
+}, { freezeTableName: true });
+
+exports.db.sync();
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
+var exports = module.exports = {};
 var express = require('express');
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('prod', process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DATABASE_URL,
-  dialect: 'postgres'
-});
 var queries = require('./db/queries.js');
-var schema = require('./db/schema.js');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
